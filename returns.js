736818da727d34d1a03262b40ee01f1b3f95a13e
soooi <blablah825@gmail.com>
2021-09-14 21:01:51 +0900

    return  값 확인 및 출력

--- returns.js
+++ returns.js
@@ -1,7 +1,63 @@
-const age = 96;
+const age = 30;
 function calculateKrAge(ageOfForeigner){
-    return ageOfForeigner +2;
-
+    ageOfForeigner +2;
+    return "hello" 
 }
 const krAge = calculateKrAge(age);
-console.log(krAge)
+console.log("krAge= "+krAge);
+
+
+const calculator = {
+    plus : function(a, b){
+        return a+b;
+    },
+
+    minus : function(a, b){
+        return a-b;
+    },
+
+    times : function(a, b){
+        return a*b;
+    },
+
+    divide : function(a, b){
+        return a/b;
+    },
+    power: function(a,b){
+        return a**b;
+    },
+};
+
+const plusResult = calculator.plus(2,3);
+console.log("plusResult = "+ plusResult)
+
+
+function counter(){
+    for(var count =1; ; count++){
+        console.log( count+"A");
+        if( count === 3){
+            return;
+        }
+        console.log(count+"B");
+    }
+}
+
+counter();
+
+
+function plus(a,b) {
+    return a+b;
+ }
+ const demo = plus(5,15);
+ console.log("demo =" +demo)
+
+ function magic(x) {
+    return function calc(x) { return x * 42 };
+  }
+  
+  var answer = magic(1337);
+
+  console.log(answer)
+
+  document.getElementsByTagName('h1').innerHTML
+ 
