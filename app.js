35bfb836c5ae0023d169fd9a48028126e84b8562
soooi <blablah825@gmail.com>
2021-09-16 20:58:59 +0900

    app.js 함수명의 위치를 바꾸는것과, addEventListener 선택자에서 null값을 불러올때--- html에서 태그가 div인지 확인해  볼 것,

--- app.js
+++ app.js
@@ -1,3 +1,45 @@
-const tagP = document.getElementById('appjs');
-console.log("tagP = "+tagP)
-tagP.innerHTML = "아니 진짜"
+const titleID = document.getElementById('appjs');
+const titleCN = document.getElementsByClassName('title');
+const mainCN = document.querySelector('.hello');
+const subTag = document.querySelector('.hello h1');
+
+console.log("titleID = "+titleID.id)
+console.log("titleCN = "+titleCN.className)
+console.log("mainCN = "+mainCN.className);
+
+
+titleID.innerHTML = "아니 진짜"
+console.dir(subTag)
+
+
+/* 
+function handleMouseEnter(){
+    alert("ddd")
+    subTag.style.color="green";
+}
+
+function handleMouseLeave(){
+    console.log("sdfjdksf")
+    subTag.style.color="blue";
+}
+
+function handleTitleClick(){
+    console.log("title was clicked!");
+    subTag.style.color="red";
+}
+*/
+const fncEvent = {
+    handleTitleClick:function(){
+        alert("클릭했오");
+    },
+    handleMouseLeave:function(){
+        alert("해당영역을 벗어났오");
+    }
+}
+
+
+subTag.addEventListener("click", fncEvent.handleTitleClick);
+/*
+subTag.addEventListener("mouseenter", handleMouseEnter);
+subTag.addEventListener("mouseleave", handleMouseLeave);
+*/
