fa67af69b472d8d20f3bdb23f81ac65d56f4814a
soozin-L <blablah825@gmail.com>
2021-09-26 01:21:43 +0900

    삭제

--- js/todo.js
+++ js/todo.js
@@ -2,18 +2,27 @@ const todoForm = document.getElementById("todo-form");
 const todoFormInput = todoForm.querySelector("input");
 const todoList = document.getElementById("todo-list");
 
+function deleteTodo(event){    
+    const li = event.target.parentElement;
+    li.remove;
+}
 
 function paintTodo(newTodo){
     const li = document.createElement("li");
     const span = document.createElement("span");
-
-    li.appendChild(span);
-    todoList.appendChild(li);
+    const btn = document.createElement("button");
     span.innerText=newTodo;
+
+    btn.innerText = "❌"
+    btn.addEventListener("click", deleteTodo);
+    
+    li.appendChild(span);
+    li.appendChild(btn);
+    todoList.appendChild(li);
 }; 
 
 function handleTodoSubmit(event){
-    event.preventDefault(); preventDefault
+    event.preventDefault(); 
     const newTodo = todoFormInput.value;
     console.log(newTodo)
     todoFormInput.value ="";
